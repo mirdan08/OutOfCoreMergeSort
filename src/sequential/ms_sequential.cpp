4a896fdc799954e3aba7256b97c9488c4e41adc1
mirdan08 <mirdan0825@gmail.com>
2025-06-08 14:45:14 +0200

    added buffering

--- src/sequential/ms_sequential.cpp
+++ src/sequential/ms_sequential.cpp
@@ -16,63 +16,75 @@ int main(int argc,char*argv[]){
     bool success=parse_cli_args(argc,argv,records_num,threads_num,verbose);
 
     if(!success){
-        std::cout << "exiting" << std::endl;
+        std::cout << "error: wrong arguments.\nExiting..." << std::endl;
         return 1;
     }
 
     auto start_time = std::chrono::high_resolution_clock::now();
     
-    std::ifstream in_file("input.pms",std::ifstream::binary);
+    std::ifstream in_file("input.pms",std::ifstream::binary | std::ios::ate);
+    std::streamsize file_size= in_file.tellg();
+    in_file.seekg(0);
     uint64_t max_file_payload_size;
     in_file.read(reinterpret_cast<char*>(&max_file_payload_size),sizeof(uint64_t));    
-    std::cout << max_file_payload_size << std::endl;
     if(max_file_payload_size != PAYLOAD_MAX){
-        std::cout<< "error:the maximum payload size should correspond to " << PAYLOAD_MAX << " but it isn't\nExiting..." << std::endl;
+        std::cout<< "error:the maximum payload size should correspond to " << PAYLOAD_MAX << " but it isn't.\nExiting..." << std::endl;
         in_file.close();
         return 1;
     }
     records_num=0;
     in_file.read(reinterpret_cast<char*>(&records_num),sizeof(uint64_t));
-
-    //std::vector<uint64_t> offsets(records_num);
-    //in_file.read(reinterpret_cast<char*>(offsets.data()),sizeof(uint64_t)*records_num);
+    
+    std::vector<uint64_t> offsets(records_num);
+    in_file.read(reinterpret_cast<char*>(offsets.data()),sizeof(uint64_t)*records_num);
+    std::cout << "starting with:\n" 
+              << "\tnumber of records:\t" << records_num 
+              << "\tmax payload size:\t" << max_file_payload_size << std::endl;
 
     const unsigned int header_offset=in_file.tellg();
 
     unsigned long records_count=0;
     size_t current_size=0;
     std::vector<PosKeyPair> pos_key_data;
-    const unsigned int record_size=PAYLOAD_MAX+sizeof(uint32_t)+sizeof(uint64_t);
-    const unsigned int buffers_num= (record_size*records_num+MAX_MEMORY_LIMIT-1)/MAX_MEMORY_LIMIT;
-    const unsigned int buffer_size= std::min(MAX_MEMORY_LIMIT,record_size*records_num);
-    char* buffer=new char[buffer_size] ;
+    const unsigned int max_record_size=PAYLOAD_MAX+sizeof(uint32_t)+sizeof(uint64_t);
+    const unsigned int buffer_size= std::min(MAX_MEMORY_LIMIT,max_record_size*records_num);
+    char* buffer=new char[buffer_size];
+    const unsigned int payload_header_size=sizeof(uint32_t)+sizeof(uint64_t);
+    const unsigned int file_header_size=sizeof(uint64_t)+sizeof(uint64_t);
+    const unsigned int offset_header_size=sizeof(uint64_t)*records_num;
     // we read only keys and store the indexes to apply std::sort
-    for(int i=0;i<buffers_num;++i){
-        unsigned int buffer_start=i*buffer_size;
+    unsigned int buffer_offset=0;
+    unsigned int record_offset=0;
+    while(records_count<records_num){
+        unsigned int buffer_start=buffer_offset;
         //avoid reading over the buffer length
-        unsigned int buffer_length=std::min(buffer_size,record_size*(uint)records_num-buffer_start);
+        unsigned int buffer_length=std::min(buffer_size,(uint)file_size-(uint)(file_header_size) -buffer_start);
+        in_file.seekg(file_header_size+offset_header_size+buffer_offset);
         in_file.read(buffer,buffer_length);
-        unsigned int bytes_read=0;
+        
         //Note: header is not read in the buffer
-        while( bytes_read< buffer_length ){
+        //If the offset exceed the payload header size stop
+        while( record_offset + payload_header_size< buffer_length && records_count < records_num){
             PosKeyPair pkp;
-            std::memcpy(&pkp.key,buffer+records_count*record_size,sizeof(uint64_t));
+            uint32_t payload_len=0;
+            std::memcpy(&payload_len,buffer+record_offset,sizeof(uint32_t));
+            std::memcpy(&pkp.key,buffer+record_offset+sizeof(uint32_t),sizeof(uint64_t));
             pkp.pos=records_count;
             pos_key_data.push_back(std::move(pkp));
-            records_count++;
-            bytes_read+=record_size;
+            record_offset+=payload_len+payload_header_size;
+            ++records_count;
         }
+        buffer_offset=record_offset;
+        record_offset=0;
     }
     in_file.close();
     delete[] buffer;
 
-    std::cout << "starting with:\n" 
-              << "\tnumber of records:\t" << records_num 
-              << "\tmax payload size:\t" << PAYLOAD_MAX << std::endl;
     std::sort(
         pos_key_data.begin(),pos_key_data.end(),
         [](const PosKeyPair& a,const PosKeyPair& b){return a.key<b.key;}
     );
+
     if (verbose){
         unsigned int i=0;
         for(const auto& pkp:pos_key_data){
