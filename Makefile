66e9c0d8c7d59d82689128f8583515ebffffd927
mirdan08 <mirdan0825@gmail.com>
2025-06-14 16:58:48 +0200

    added openmp implementation stub

--- Makefile
+++ Makefile
@@ -16,10 +16,12 @@ TARGETS    = ms_sequential ff_singlenode openmp_singlenode mpi_multinode payload
 CORE_SRC   = src/core/core.cpp
 SEQ_SRC    = src/sequential/ms_sequential.cpp
 FF_SRC 	   = src/ff_singlenode/ms_ff_singlenode.cpp
+OMP_SRC 	   = src/ff_singlenode/openmp_ff_singlenode.cpp
 UTILS_SRC    = src/utils/utils.cpp
 # Object files
 CORE_OBJ = $(CORE_SRC:.cpp=.o)
 FF_OBJ = $(FF_SRC:.cpp=.o)
+OMP_OBJ = $(OMP_SRC:.cpp=.o)
 UTILS_OBJ = $(UTILS_SRC:.cpp=.o)
 SEQ_OBJ = $(SEQ_SRC:.cpp=.o)
 
@@ -33,6 +35,8 @@ ms_sequential: $(SEQ_OBJ) $(CORE_OBJ) $(UTILS_OBJ)
 	$(CXX) $(CXXFLAGS) -DRPAYLOAD_MAX=$(RPAYLOAD_MAX) $(INCLUDES) $(OPTFLAGS) -o $@ $^ $(LDFLAGS)
 ff_singlenode: $(FF_OBJ) $(CORE_OBJ) $(UTILS_OBJ)
 	$(CXX) $(CXXFLAGS) -DRPAYLOAD_MAX=$(RPAYLOAD_MAX) $(INCLUDES) $(OPTFLAGS) -o $@ $^ $(LDFLAGS)
+openmp_singlenode: $(OMP_OBJ) $(CORE_OBJ) $(UTILS_OBJ)
+	$(CXX) $(CXXFLAGS) -DRPAYLOAD_MAX=$(RPAYLOAD_MAX) $(INCLUDES) $(OPTFLAGS) -o $@ $^ $(LDFLAGS) -omp
 
 payload_generator:utilities/payload_generator.cpp 
 	$(CXX) $(CXXFLAGS) $(INCLUDES) $(OPTFLAGS) -o utilities/$@ $^ $(LDFLAGS)
---
+++ src/openmp_singlenode/openmp_singlenode.cpp
@@ -0,0 +1,109 @@
+#include <core/core.hpp>
+#include <utils/utils.hpp>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <memory>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
+using PosKeyVec=std::vector<PosKeyPair>;
+
+using IndexPair=std::pair<unsigned long,unsigned long>;
+using SortResult=std::tuple<unsigned long,unsigned long,unsigned long>;
+
+int main(int argc,char*argv[]){
+    uint64_t records_num=0;
+    size_t threads_num=0;
+    bool verbose = false;
+    std::string filename="";
+    bool success=parse_cli_args(argc,argv,threads_num,verbose,filename);
+    if(!success){
+        std::cout << "Exiting..." << std::endl;
+        return 1;
+    }
+    if(filename==""){
+        std::cout << "please specify filename" << std::endl;
+        return 1;
+    }
+    auto start_time = std::chrono::high_resolution_clock::now();
+    
+    std::ifstream in_file(filename,std::ifstream::binary | std::ios::ate);
+    std::streamsize file_size= in_file.tellg();
+    in_file.seekg(0);
+    uint64_t max_file_payload_size;
+    in_file.read(reinterpret_cast<char*>(&max_file_payload_size),sizeof(uint64_t));    
+    if(max_file_payload_size != payload_max){
+        std::cout<< "error:the maximum payload size should correspond to " << payload_max << " but it isn't.\nExiting..." << std::endl;
+        in_file.close();
+        return 1;
+    }
+    records_num=0;
+    in_file.read(reinterpret_cast<char*>(&records_num),sizeof(uint64_t));
+    
+    std::vector<uint64_t> offsets(records_num);
+    in_file.read(reinterpret_cast<char*>(offsets.data()),sizeof(uint64_t)*records_num);
+    std::cout << "starting with:\n" 
+              << "\tnumber of records:\t" << records_num 
+              << "\tmax payload size:\t" << max_file_payload_size << std::endl;
+
+    const unsigned int header_offset=in_file.tellg();
+    unsigned long records_count=0;
+    size_t current_size=0;
+    std::vector<PosKeyPair> pos_key_data;
+    const unsigned int max_record_size=payload_max+sizeof(uint32_t)+sizeof(uint64_t);
+    const unsigned int buffer_size= std::min(MAX_MEMORY_LIMIT,max_record_size*records_num);
+    char* buffer=new char[buffer_size];
+    const unsigned int payload_header_size=sizeof(uint32_t)+sizeof(uint64_t);
+    const unsigned int file_header_size=sizeof(uint64_t)+sizeof(uint64_t);
+    const unsigned int offset_header_size=sizeof(uint64_t)*records_num;
+    // we read only keys and store the indexes to apply std::sort
+    unsigned int buffer_offset=0;
+    unsigned int record_offset=0;
+    while(records_count<records_num){
+        unsigned int buffer_start=buffer_offset;
+        //avoid reading over the buffer length
+        unsigned int buffer_length=std::min(buffer_size,(uint)file_size-(uint)(file_header_size) -buffer_start);
+        in_file.seekg(file_header_size+offset_header_size+buffer_offset);
+        in_file.read(buffer,buffer_length);
+        //Note: header is not read in the buffer
+        //If the offset exceed the payload header size stop
+        while( record_offset + payload_header_size< buffer_length && records_count < records_num){
+            PosKeyPair pkp;
+            uint32_t payload_len=0;
+            std::memcpy(&payload_len,buffer+record_offset,sizeof(uint32_t));
+            std::memcpy(&pkp.key,buffer+record_offset+sizeof(uint32_t),sizeof(uint64_t));
+            pkp.pos=records_count;
+            pos_key_data.push_back(std::move(pkp));
+            record_offset+=payload_len+payload_header_size;
+            ++records_count;
+        }
+        buffer_offset=record_offset;
+        record_offset=0;
+    }
+    in_file.close();
+    delete[] buffer;
+    if (verbose){
+        unsigned int i=0;
+        for(const auto& pkp:pos_key_data){
+            std::cout<< i++ << "\t[" << pkp.pos << ":" << pkp.key << "]" << std::endl;
+        }
+    }
+    size_t num_workers=threads_num;
+    PosKeyVec result;
+
+    //openmp implementation
+    auto end_time = std::chrono::high_resolution_clock::now();
+    if (verbose){
+        unsigned int i=0;
+        for(const auto& pkp:result){
+            std::cout<< i++ << "\t[" << pkp.pos << ":" << pkp.key << "]" << std::endl;
+        }
+    }
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout<< "time(ms):" << duration.count() << std::endl;
+
+    return 0;
+}
