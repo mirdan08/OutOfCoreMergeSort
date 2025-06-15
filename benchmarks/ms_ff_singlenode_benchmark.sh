211a8e495a6fac6fb4e6e139d6dae9967a89a843
mirdan08 <mirdan0825@gmail.com>
2025-06-15 10:37:21 +0200

    added ff single node benchmark

---
+++ benchmarks/ms_ff_singlenode_benchmark.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+trials=5
+# 128b 1Gb 2Gb
+n_records=($((128)) $((1*1024*1024)) )
+# 64b 1Kb 1Mb
+max_payloads=($((1024)) $((1024*1024)) )
+
+num_threads=(1 2 4 8 16 32)
+
+make cleanall
+make payload_generator
+if ! [ -f "test_files" ]; then
+    mkdir -p test_files
+fi
+
+for mp in "${max_payloads[@]}"; do
+    for nr in "${n_records[@]}"; do
+    if ! [ -f "test_files/file_mp${mp}_nr${nr}.pms" ]; then
+        ./utilities/payload_generator -o test_files/file_mp${mp}_nr${nr}.pms -p $mp -r $nr -v 0
+    fi
+    echo "test_files/file_mp${mp}_nr${nr}.pms already exists"
+    done
+done
+
+echo "files generated!"
+echo "starting experiments:"
+
+output_file="$1"
+touch "$output_file"
+echo "" > "$output_file"
+echo "iteration,num_threads,max_payload_size,records_number,time(ms)" >> "$output_file"
+
+for i in $(seq 1 $trials); do
+    for mp in "${max_payloads[@]}"; do
+        for nr in "${n_records[@]}"; do
+            for nt in "${num_threads[@]}"; do
+                make cleanall
+                make RPAYLOAD_MAX=$mp ff_singlenode
+                output=$(./ff_singlenode -i test_files/file_mp${mp}_nr${nr}.pms -t ${nt} -v 0)
+                echo "iteration=$i max_payload=$mp records_number=$nr"
+                echo "$output"
+                time_ms=$(echo "$output" | grep 'time(ms):' | awk -F ':' '{print $2}')
+                echo "$i,$nt,$mp,$nr,$time_ms" >> "$output_file"
+            done
+        done
+    done
+done
+
+echo "experiments done!"
